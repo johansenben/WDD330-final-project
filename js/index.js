465842a3c59898c79f4c51c96b8ea7c27bc24f8a
Ben Johansen <johansenben12@gmail.com>
2025-04-14 14:22:47 -0600

    week 6 v1

--- js/index.js
+++ js/index.js
@@ -2,5 +2,39 @@ import { getCityFromLocation, getLocationFromIP } from "./location.js";
 import { getWeatherData } from "./weather.js";
 
 //console.log(await getLocationFromIP());
-//console.log(await getCityFromLocation(-112,49))
-console.log(await getWeatherData(-112.8 + '',49.7 + ''))
+//console.log(await getWeatherData(-112.8,49.7))
+//console.log(await getCityFromLocation(-112.8,49.7))
+
+document.querySelector("#use-my-location").addEventListener("change", e => {
+    const checked = e.target.checked;
+    document.querySelector("#latitude").required = !checked;
+    document.querySelector("#longitude").required = !checked;
+});
+const displayData = (useUsersLocation, location, city, weatherData) => {
+    if ([useUsersLocation, location, city, weatherData].includes(undefined))
+        return;
+    document.querySelector("#weather-data").innerHTML = `
+        <h2>Weather in ${city}</h2>
+        <h3>Latitude: <span>${location.latitude}</span></h3>
+        <h3>Longitude: <span>${location.longitude}</span></h3>
+        <br>
+        <h3>Current Temperature: <span>${weatherData.current.temp_c} &deg;C or ${weatherData.current.temp_f}&deg;F</span></h3>
+        <h3>Maximum Temperature: <span>${weatherData.forecast.forecastday[0].day.maxtemp_c} &deg;C or ${weatherData.forecast.forecastday[0].day.maxtemp_f}&deg;F</span></h3>
+        <h3>Minimum Temperature: <span>${weatherData.forecast.forecastday[0].day.mintemp_c} &deg;C or ${weatherData.forecast.forecastday[0].day.mintemp_f}&deg;F</span></h3>
+        <h3>Average Temperature: <span>${weatherData.forecast.forecastday[0].day.avgtemp_c} &deg;C or ${weatherData.forecast.forecastday[0].day.avgtemp_f}&deg;F</span></h3>
+
+    `;
+}
+document.querySelector("#get-weather-form").addEventListener("submit", async e => {
+    e.preventDefault();
+    const useUsersLocation = e.target.querySelector("#use-my-location").checked;
+    const formData = new FormData(e.target);
+    const values = Object.fromEntries(formData.entries());
+    const location = useUsersLocation ? await getLocationFromIP() : { longitude: values.longitude, latitude: values.latitude };
+    const city = await getCityFromLocation(location.longitude, location.latitude);
+    const weatherData = await getWeatherData(location.longitude, location.latitude);
+    displayData(useUsersLocation, location, city, weatherData);
+});
+const APIalert = () => {
+
+}
--- js/location.js
+++ js/location.js
@@ -1,11 +1,12 @@
 import { getData } from "./api.js";
 
 const IPGeolocationRoute = "https://api.geoapify.com/v1/ipinfo?apiKey=0fb41ebb271d497db727ebc870f97033";
-const getPlacesRoute = (lon, lat) => `https://api.geoapify.com/v1/geocode/reverse?apiKey=0fb41ebb271d497db727ebc870f97033&lat="${lat}&lon=${lon}"&layers=city`;
+const getPlacesRoute = (lon, lat) => `http://api.weatherapi.com/v1/forecast.json?key=cf23cb44b6074560895183203250704&q=${lat},${lon}`;
 
 export const getLocationFromIP = async () => {
     return getData(IPGeolocationRoute)
         .then(res => res.json())
+        .then(json => json.location)
         .catch(error => {
             console.error(error)
         });
@@ -15,6 +16,12 @@ export const getCityFromLocation = async (longitude, latitide) => {
     //route doesn't work; it thinks the nearest city to Lethbridge, AB is in Montana
     return getData(getPlacesRoute(longitude, latitide))
         .then(res => res.json())
+        .then(json => json.location)
+        .then(location => {
+            if (!location || [location?.name, location?.region, location?.country].includes(undefined))
+                throw new Error("Can't find City name");
+            return `${location.name}, ${location.region}, ${location.country}`;
+        })
         .catch(error => {
             console.error(error)
         });
